857c02bdbe4e3bedfadae0f6793612d03e7d64e3
Anastasiya Andronova <anastasiya5.88@mail.ru>
2024-01-25 23:50:54 +0300

    feat: add solution of nodejs-basics

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,26 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+  const folderPath = path.join(__dirname, 'files');
+  fs.access(folderPath, (err) => {
+    if (err) {
+      console.log("FS operation failed: Files folder doesn't exist");
+    } else {
+      fs.readdir(folderPath, (error, files) => {
+        if (error) return console.log(error);
+        const arrayOfFileNames = []
+        files.forEach((file) => {
+          arrayOfFileNames.push(file.valueOf());
+        });
+        console.log(arrayOfFileNames);
+      });
+    }
+  });
 };
 
 await list();
