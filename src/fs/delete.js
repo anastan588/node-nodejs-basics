809edf125074a36553b6ebcb1e85cca629be0dc1
Anastasiya Andronova <anastasiya5.88@mail.ru>
2024-01-25 23:37:23 +0300

    feat: add solution of delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,23 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+    const fileToRemove = path.join(__dirname, 'files','fileToRemove.txt');
+    fs.access(fileToRemove, (err) => {
+        if (err) {
+          console.log("FS operation failed: File 'fileToRemove.txt' doesn't exist");
+        } else {
+            fs.unlink(fileToRemove, (error) => {
+                if (error) return console.log(error); 
+                console.log("File 'fileToRemove.txt' has been deleted");
+              });;
+        }
+      });
 };
 
 await remove();
