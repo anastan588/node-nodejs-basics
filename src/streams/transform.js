17a8adabf5ac4c45d91a56e6d7b959d2d176c087
Anastasiya Andronova <anastasiya5.88@mail.ru>
2024-01-27 19:22:13 +0300

    feat: add solution of streams write task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,29 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const transform = async () => {
-    // Write your code here 
+    const fileToWrite = path.join(__dirname, 'files', 'fileToWrite.txt');
+  const stream = fs.createWriteStream(fileToWrite, { flags: 'a' });
+  // process.stdout.write('Hello, friend) Write something\n');
+  // process.stdout.write('Please: write something here:  ');
+  process.stdin.on('data', (data) => {
+    let information = data.toString().trim();
+    console.log(information);
+    stream.write(`${information}\n`);
+    // stream.end();
+    // console.log('Data written to file.');
+    
+    // process.exit();
+  });
+  process.stdin.on('end', () => {
+    stream.end();
+    console.log('Data written to file.');
+    process.stdout.write('Good bye! \n');
+  });
 };
 
 await transform();
