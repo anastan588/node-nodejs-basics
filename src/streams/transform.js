d4ab92ab2cd73819f7062942b2e63be17d94ddb7
Anastasiya Andronova <anastasiya5.88@mail.ru>
2024-01-27 19:48:44 +0300

    feat: add solution of strems transforms task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,29 +1,14 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-import fs from 'fs';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { Transform } from 'stream';
 
 const transform = async () => {
-    const fileToWrite = path.join(__dirname, 'files', 'fileToWrite.txt');
-  const stream = fs.createWriteStream(fileToWrite, { flags: 'a' });
-  // process.stdout.write('Hello, friend) Write something\n');
-  // process.stdout.write('Please: write something here:  ');
-  process.stdin.on('data', (data) => {
-    let information = data.toString().trim();
-    console.log(information);
-    stream.write(`${information}\n`);
-    // stream.end();
-    // console.log('Data written to file.');
-    
-    // process.exit();
+  const reverseTransform = new Transform({
+    transform(chunk, encoding, callback) {
+      const reversedChunk = chunk.toString().split('').reverse().join('');
+      this.push(reversedChunk);
+      callback();
+    },
   });
-  process.stdin.on('end', () => {
-    stream.end();
-    console.log('Data written to file.');
-    process.stdout.write('Good bye! \n');
-  });
+  process.stdin.pipe(reverseTransform).pipe(process.stdout);
 };
 
 await transform();
