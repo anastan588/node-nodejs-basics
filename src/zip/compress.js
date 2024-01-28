e54eb8e646686a41331d9a070c6605100057318a
Anastasiya Andronova <anastasiya5.88@mail.ru>
2024-01-28 12:27:17 +0300

    feat: add solution of compress task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import zlib from 'zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+  const fileToCompress = path.join(__dirname, 'files', 'fileToCompress.txt');
+  const streamToCompress = fs.createReadStream(fileToCompress);
+  const fileToOut = path.join(__dirname, 'files', 'archive.gz');
+  const streamToOut = fs.createWriteStream(fileToOut);
+  const gzip = zlib.createGzip();
+  streamToCompress.pipe(gzip).pipe(streamToOut);
+  streamToOut.on('finish', () => {
+    console.log('Compression completed successfully.');
+  });
 };
 
 await compress();
