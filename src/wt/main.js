33d8112b47accdc2f2b792b92bced15940e38e18
Anastasiya Andronova <anastasiya5.88@mail.ru>
2024-01-29 22:27:28 +0300

    feat: add solution of wt tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,50 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import os from 'os';
+import { Worker } from 'worker_threads';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const workerDataPath = path.join(__dirname, 'worker.js');
+
 const performCalculations = async () => {
-    // Write your code here
+  const logicalCores = os.cpus().length;
+  const workerCount = logicalCores;
+  const workersArray = new Array(workerCount).fill(10);
+  workersArray.reduce((acc, item, index) => {
+    const value = item + index;
+    workersArray[index] = value;
+    return value;
+  }, 10);
+  console.log(workersArray);
+  const workers = workersArray.map((item) => {
+    return new Worker(workerDataPath, { workerData: item });
+  });
+  let requests = workers.map((worker) => {
+    return new Promise((resolve, reject) => {
+      worker.on('message', resolve);
+      worker.on('error', reject);
+    });
+  });
+  Promise.allSettled(requests).then((responses) => {
+    const resultArray = [];
+    responses.forEach((response) => {
+      const resultObject = {
+        status: '',
+        data: 0,
+      };
+      if (response.status === 'fulfilled') {
+        resultObject.status = 'resolved';
+        resultObject.data = response.value;
+      }
+      if (response.status === 'rejected') {
+        resultObject.status = 'error';
+        resultObject.data = null;
+      }
+      resultArray.push(resultObject);
+    });
+    console.log(resultArray);
+  });
 };
 
 await performCalculations();
